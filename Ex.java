68ee3da70b68b6b726f741c82a3ecded6d9d3286
yhy5847 <yhy5847@gmail.com>
2022-06-20 17:09:48 +0900

    22-6-20 ver.1.3

--- Ex.java
+++ Ex.java
@@ -3,7 +3,6 @@ import java.util.ArrayList;
 
 public class Ex {
     public static void main(String[] args) {
-        
-
+       
     }
 }
--- pack03/Ask.java
+++ pack03/Ask.java
@@ -4,13 +4,13 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Ask {
-
-    Scanner input = new Scanner(System.in);
-    String ask;
-    ArrayList<String> ansList = new ArrayList();
-
-    for(int i = 0; i < 5; ++i) {
-        System.out.println(i+1 + "번째 질문을 입력해주세요.");
-        ansList.add(input.nextLine());
+    void ask5() {
+        Scanner input = new Scanner(System.in);
+        ArrayList<String> askList = new ArrayList();
+        
+        for(int i = 0; i < 5; ++i) {
+            System.out.println(i+1 + "번째 질문을 입력해주세요.");
+            askList.add(input.nextLine());
+        }
     }
 }
