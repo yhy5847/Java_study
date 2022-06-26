d14924a46017e27f91c4cf79693411f639526c90
yhy5847 <yhy5847@gmail.com>
2022-06-26 16:48:21 +0900

    22-6-26 ver.1.3

--- VendingMachine/Machine.java
+++ VendingMachine/Machine.java
@@ -2,15 +2,16 @@ package VendingMachine;
 
 public class Machine {
     int inputMoney;
-
+    int output;
 
     void outputMoney(int money, int price) {
         this.inputMoney = money;
-        System.out.println(money+"원을 넣었습니다.");
-        if(money - price >= 0) {
-            int output = money - price;
+        int output = money - price;
+        this.output = output;
+
+        if(output >= 0) {
             System.out.println(output + "원을 반환합니다.");
-        } else if(money - price < 0) {
+        } else if(output < 0) {
             System.out.println("잔액이 부족합니다.");
         }
     }
--- VendingMachine/Starter.java
+++ VendingMachine/Starter.java
@@ -6,20 +6,48 @@ public class Starter {
     
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         Drink coke = new Cola("coke", 1000, "sweet", true);
         Drink sprite = new Sprite("sprite", 900, "sweet", true);
+
         Machine machine = new Machine();
 
         System.out.println("금액을 투입하여 주십시오.");
         int money = sc.nextInt();
-        System.out.println("원하시는 음료를 선택해주세요");
-        System.out.println("1. " + coke.name + " 가격 : " coke.price)
-        System.out.println("2. " + sprite.name + " 가격 : " sprite.price)
-        int drinkNum = sc.nextInt();
-        if(drinkNum == 1) {
-            machine.outputMoney(money, coke.price);
-        } else if(drinkNum == 2) {
-            machine.outputMoney(money, sprite.price);
-        }
+
+        do {System.out.println("===============================================");
+            System.out.println("원하시는 음료를 선택해주세요");
+            System.out.println("1. " + coke.name + " 가격 : " + coke.price);
+            System.out.println("2. " + sprite.name + " 가격 : " + sprite.price);
+            System.out.println("===============================================");
+
+            int drinkNum = sc.nextInt();            
+            if(drinkNum == 1) {
+                System.out.println(coke.name+ "구입");
+
+                machine.outputMoney(money, coke.price);
+
+                if(machine.output < 0) {
+                    break;
+                }
+
+                System.out.println("이용해주셔서 감사합니다.");
+                break;
+            }else if(drinkNum == 2) {
+                System.out.println(sprite.name+ "구입");
+
+                machine.outputMoney(money, sprite.price);
+
+                if(machine.output < 0) {
+                    break;
+                }
+
+                System.out.println("이용해주셔서 감사합니다.");
+
+                break;
+            }else {
+                System.out.println("다시 입력해주세요");
+            }
+        }while(true);
     }
 }
