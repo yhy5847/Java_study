734fb5cb15cef441fbdd015126de73ed196c5f1e
yhy5847 <yhy5847@gmail.com>
2022-07-21 17:26:04 +0900

    22-7-21 ver.1

--- TestFile/main.java
+++ TestFile/main.java
@@ -1,10 +1,16 @@
 package TestFile;
 
+import java.util.HashMap;
+
 public class main {
 
     public static void main(String[] args) {
 
-        Sig.getInstance().test;
+        HashMap<Integer,String> map = new HashMap();
+        map.put(1, "banana");
+        map.put(2, "apple");
+
+        System.out.println(map);
 
 
     }
---
+++ TestFile/project/Main.java
@@ -0,0 +1,9 @@
+package TestFile.project;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("프로그램이 정상적으로 실행됩니다 양호열");
+
+    }
+}
